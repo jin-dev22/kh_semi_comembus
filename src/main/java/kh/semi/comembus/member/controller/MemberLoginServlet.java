23657601cc80192f701dfd209f74952f8de157a3
misongko <misong.ko23@gmail.com>
2022-07-14 15:56:18 +0900

    MemberLoginServlet.java - MemberService와 연결
    MemberService의 findById메소드을 통해 아이디 값 전달해서 회원정보 가져옴

--- src/main/java/kh/semi/comembus/member/controller/MemberLoginServlet.java
+++ src/main/java/kh/semi/comembus/member/controller/MemberLoginServlet.java
@@ -8,9 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kh.semi.comembus.member.model.dto.Member;
+import kh.semi.comembus.member.model.service.MemberService;
+
 @WebServlet("/member/login")
 public class MemberLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private MemberService memberService = new MemberService();
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -27,16 +31,24 @@ public class MemberLoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-	
-		String memberId = request.getParameter("memberId");
-		String password = request.getParameter("password");
-		String location = request.getParameter("location");
-		System.out.println("memberId = " + memberId);
-		System.out.println("password = " + password);
-		System.out.println("location = " + location);
-		 
-		response.sendRedirect(location);
+		try {
+			request.setCharacterEncoding("utf-8");
+			
+			String memberId = request.getParameter("memberId");
+			String password = request.getParameter("password");
+			String location = request.getParameter("location");
+			System.out.println("memberId = " + memberId);
+			System.out.println("password = " + password);
+			System.out.println("location = " + location);
+			
+			Member member = memberService.findById(memberId);
+			System.out.println("member@MemberLoginServlet = " + member); 
+			 
+			response.sendRedirect(location);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
 		
 	}
 
